4a5e5aeff505c23a0b777ebd62f4ffb15ea3e8e5
leon <slzhang1983@gmail.com>
2013-03-18 19:28:56 +0800

    change base 64

--- pisces/util/token.py
+++ pisces/util/token.py
@@ -7,10 +7,11 @@ import mcrypt
 from config import *
 
 def token_to_uid(token):
-    data = base64.decodestring(token)
+    data = token.replace('.', '+').replace('_', '/') + '='
+    data = base64.decodestring(data)
     mc = mcrypt.MCRYPT(MCRYPT_ALGORITHM, MCRYPT_MODE)
     mc.init(MCRYPT_KEY)
-    val = mc.encrypt(data)
+    val = mc.decrypt(data)
     return int(val)
 
 def uid_to_token(uid):
@@ -19,6 +20,8 @@ def uid_to_token(uid):
         data = '0' * (20 - len(data)) + data
     mc = mcrypt.MCRYPT(MCRYPT_ALGORITHM, MCRYPT_MODE)
     mc.init(MCRYPT_KEY)
-    val = mc.decrypt(data)
-    return base64.encodestring(val)
+    val = mc.encrypt(data)
+    val = base64.encodestring(val)
+    val = val.replace('+', '.').replace('/', '_').replace('=', '')
+    return val
 
