1dc1a293a182e7f4d93f330e83fa341232ca3500
leon <slzhang1983@gmail.com>
2013-03-24 12:41:05 +0800

    change main

--- pisces/main.py
+++ pisces/main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys
+import os
+import os.path
+import profile
+import pstats
+
 from app import App
 
 def main():
     App().run()
-    
+
 if __name__ == '__main__':
     main()
